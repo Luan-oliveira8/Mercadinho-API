f7e2df237dd009a25e436fa71e2f623e85e9e3bc
Luan-Fones <luanfones@gmail.com>
2024-11-20 16:04:42 -0300

    Adicionando express e server inicial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Teste");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Rodando na porta ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("Teste");
+});
