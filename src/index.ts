aa66baf585a331eaafec976d71d87f0d916ce274
Luan-Fones <luanfones@gmail.com>
2024-11-21 20:22:52 -0300

    Adicionando MongoClient e mongodb connection

--- src/index.ts
+++ src/index.ts
@@ -2,24 +2,31 @@ import express from "express";
 import { config } from "dotenv";
 import { GetUsersController } from "./controllers/getUsers/getUsers";
 import { MongoGetUsersRepository } from "./repositories/getUsers/mongoGetUsers";
+import { MongoClient } from "./database/mongo";
 
-config();
+const main = async () => {
+  config();
 
-const app = express();
+  const app = express();
 
-const port = process.env.PORT || 8080;
+  await MongoClient.connect();
 
-app.listen(port, () => console.log(`Rodando na porta ${port}`));
+  app.get("/", (req, res) => {
+    res.send("Teste");
+  });
 
-app.get("/", (req, res) => {
-  res.send("Teste");
-});
+  app.get("/users", async (req, res) => {
+    const mongoGetUsersRepository = new MongoGetUsersRepository();
+    const getUsersController = new GetUsersController(mongoGetUsersRepository);
 
-app.get("/users", async (req, res) => {
-  const mongoGetUsersRepository = new MongoGetUsersRepository();
-  const getUsersController = new GetUsersController(mongoGetUsersRepository);
+    const { body, statusCode } = await getUsersController.handle();
 
-  const { body, statusCode } = await getUsersController.handle();
+    res.send(body).status(statusCode);
+  });
 
-  res.send(body).status(statusCode);
-});
+  const port = process.env.PORT || 8080;
+
+  app.listen(port, () => console.log(`Rodando na porta ${port}`));
+};
+
+main();
